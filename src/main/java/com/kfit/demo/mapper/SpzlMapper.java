8349a9f1ffb011f6bc1a256175e08cb0cfbe3104
代世平 <daishiping@prolog-int.com>
2021-05-24 15:15:34 +0800

    20210524 山东恒安医药新分支，数据库配置及SQL修改

--- src/main/java/com/kfit/demo/mapper/SpzlMapper.java
+++ src/main/java/com/kfit/demo/mapper/SpzlMapper.java
@@ -9,13 +9,11 @@ import java.util.Map;
 
 public interface SpzlMapper {
 
-	@Select("select '同云堂' as suppliers_name,ypdm as ypdm,cddm as cddm,jx as jx,CONVERT(varchar(100), scrq, 23) as scrq," +
-			"goods_id_s+'_'+production_batch as goods_id_s,goods_sn as goods_sn,drug_common_name as drug_common_name,manufacturer as manufacturer," +
-			"approve_numer as approve_number,drug_img as drug_img,specifications as specifications,package_unit as package_unit," +
-			"medium_package as medium_package,large_package as large_package,'1' as is_retail," +
-			"CONVERT(varchar(100), production_batch, 23) as production_batch,CONVERT(varchar(100), date_expiration, 23) as date_expiretion," +
-			"repertory as repertory,bar_code as bar_code,suppliers_price as supplier_price,drugid as drugid," +
-			"is_on_sale as is_on_sale,category as category from v_ware_b2b_tyt")
+	@Select("select '' AS suppliers_name,PM_ZJM as ypdm,a.cd as cddm,jx,CONVERT(varchar(100),a.SCRQ,23) as scrq,"+
+	        "a.guid as goods_id_s,a.hh as goods_sn,pm as drug_common_name,a.scdw as manufacturer,a.PZWH as approve_number,"+
+			"'' as drug_img,GG as specifications,PDW as package_unit,ZBZL as medium_package,a.mjl as large_package,'1' as is_retail,"+
+	        "PH as production_batch,CONVERT(varchar(100),a.XQ,23) as date_expiretion,a.sl as repertory,b.tm as bar_code,PFJ as suppliers_price,"+
+	        "a.guid as drugid,'1' as is_on_sale,GSPSortID as category from phk a,yw_kck b where a.hh = b.hh and a.sl > 0")
 	public List<Spbnew> getspbnew();
 
 	@Select("select '同云堂' as suppliers_name,ypdm as ypdm,cddm as cddm,jx as jx,CONVERT(varchar(100), scrq, 23) as scrq," +
