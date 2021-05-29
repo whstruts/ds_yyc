c9d4b3b1541f236e08e9028d84cfd115d75206ad
代世平 <daishiping@prolog-int.com>
2021-05-30 00:47:44 +0800

    20210529 山东恒安医药suppliers_price改为supplier_price

--- src/main/java/com/kfit/demo/mapper/SpzlMapper.java
+++ src/main/java/com/kfit/demo/mapper/SpzlMapper.java
@@ -10,7 +10,7 @@ public interface SpzlMapper {
 	@Select("select '恒安医药' AS suppliers_name,PM_ZJM as ypdm,a.cd as cddm,jx,CONVERT(varchar(100),a.SCRQ,23) as scrq,"+
 	        "a.guid as goods_id_s,a.hh as goods_sn,pm as drug_common_name,a.scdw as manufacturer,a.PZWH as approve_number,"+
 			"'' as drug_img,GG as specifications,PDW as package_unit,ZBZL as medium_package,a.mjl as large_package,'1' as is_retail,"+
-	        "PH as production_batch,CONVERT(varchar(100),a.XQ,23) as date_expiretion,a.sl as repertory,b.tm as bar_code,PFJ as suppliers_price,"+
+	        "PH as production_batch,CONVERT(varchar(100),a.XQ,23) as date_expiretion,a.sl as repertory,b.tm as bar_code,PFJ as supplier_price,"+
 	        "a.guid as drugid,'1' as is_on_sale,GSPSortID as category from phk a,yw_kck b where a.hh = b.hh and a.sl > 0")
 	public List<Spbnew> getspbnew();
 
