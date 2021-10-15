041368d3edef520ac8d53cea10138861d05e173f
代世平 <daishiping@prolog-int.com>
2021-10-16 00:58:00 +0800

    20211016 tyt

--- src/main/java/com/kfit/demo/mapper/SpzlMapper.java
+++ src/main/java/com/kfit/demo/mapper/SpzlMapper.java
@@ -24,7 +24,7 @@ public interface SpzlMapper {
 			"medium_package as medium_package,large_package as large_package,'1' as is_retail," +
 			"CONVERT(varchar(100), production_batch, 23) as production_batch,CONVERT(varchar(100), date_expiration, 23) as date_expiration," +
 			"repertory as repertory,bar_code as bar_code,suppliers_price as supplier_price,drugid as drugid," +
-			"is_on_sale as is_on_sale,category as erp_category from v_ware_b2b_tyt where goods_id_s+'_'+production_batch = #{id}")
+			"is_on_sale as is_on_sale,category as erp_category from v_ware_b2b_tyt where goods_id_s+'_'+production_batch = #{id} or drugid = #{id}")
 	public List<Spbnew> GetKCByIDNEWX(String id);
 
 	@Select("select erpCustomerID AS code,erpCustomerName AS name,cust_type AS type,taxnumber,categories from v_custom_b2b_tyt where taxnumber = #{taxnumber}")
