53d0b90d9d871d5f984409635737d2753fd2fd0f
代世平 <daishiping@prolog-int.com>
2021-12-10 11:54:24 +0800

    20211210 sdha ERP 商品过滤条件修改

--- src/main/java/com/kfit/demo/mapper/SpzlMapper.java
+++ src/main/java/com/kfit/demo/mapper/SpzlMapper.java
@@ -13,14 +13,14 @@ public interface SpzlMapper {
 	        "a.guid as goods_id_s,a.hh as goods_sn,pm as drug_common_name,a.scdw as manufacturer,a.PZWH as approve_number,"+
 			"'' as drug_img,GG as specifications,PDW as package_unit,ZBZL as medium_package,a.mjl as large_package,'1' as is_retail,"+
 	        "PH as production_batch,CONVERT(varchar(100),a.XQ,23) as date_expiration,a.sl as repertory,b.tm as bar_code,b.sj as supplier_price,"+
-	        "a.guid as drugid,'1' as is_on_sale,GSPSortID as category from phk a,yw_kck b where a.hh = b.hh and a.sl > 0 and b.bz!='含特殊药品复方制剂'")
+	        "a.guid as drugid,'1' as is_on_sale,GSPSortID as category from phk a,yw_kck b where a.hh = b.hh and a.sl > 0 and b.bz not like '%含特殊药品复方制剂%'")
 	public List<Spbnew> getspbnew();
 
 	@Select("select '恒安医药' AS suppliers_name,PM_ZJM as ypdm,a.cd as cddm,jx,CONVERT(varchar(100),a.SCRQ,23) as scrq,"+
 			"a.guid as goods_id_s,a.hh as goods_sn,pm as drug_common_name,a.scdw as manufacturer,a.PZWH as approve_number,"+
 			"'' as drug_img,GG as specifications,PDW as package_unit,ZBZL as medium_package,a.mjl as large_package,'1' as is_retail,"+
 			"PH as production_batch,CONVERT(varchar(100),a.XQ,23) as date_expiration,a.sl as repertory,b.tm as bar_code,b.sj as supplier_price,"+
-			"a.guid as drugid,'1' as is_on_sale,GSPSortID as category from phk a,yw_kck b where a.hh = b.hh and a.sl > 0 and b.bz!='含特殊药品复方制剂' and a.guid = #{id}")
+			"a.guid as drugid,'1' as is_on_sale,GSPSortID as category from phk a,yw_kck b where a.hh = b.hh and a.sl > 0 and b.bz not like '%含特殊药品复方制剂%' and a.guid = #{id}")
 	public Spbnew getspbnewById(String id);
 
 	@Select("select tjbh as code,mc as name,tel as telephone,qyfzr as linkman,yydz as address,YYZZ as taxnumber,YLJGXKZH as xkzh from gl_custom where (YYZZ = #{taxNo} or YLJGXKZH = #{taxNo}) and FDELETED=0 and STOPSELL=0")
